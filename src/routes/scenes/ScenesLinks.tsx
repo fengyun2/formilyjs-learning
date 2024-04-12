360768cf17a626b6ba7ab3e82e14a6e2f51e0728
ly2011 <1518550424@qq.com>
2024-04-12 21:09:20 +0800

    feat: Add forgot-password demo

--- src/routes/scenes/ScenesLinks.tsx
+++ src/routes/scenes/ScenesLinks.tsx
@@ -2,27 +2,33 @@ import LoginRegisterMarkupSchema from './login-register/login/markup-schema';
 import LoginRegisterJsonSchema from './login-register/login/json-schema';
 import LoginRegisterJsxDemo from './login-register/login/jsx-demo';
 import LoginRegisterRegisterMarkupSchema from './login-register/register/markup-schema';
+import LoginRegisterForgotPasswordMarkupSchema from './login-register/forgot-password/markup-schema';
 
 const ScenesLinks = [
   {
     path: '/login-register-markup-schema',
-    name: 'LoginRegisterMarkupSchema',
+    name: '登录(MarkupSchema)',
     Component: LoginRegisterMarkupSchema,
   },
   {
     path: '/login-register-json-schema',
-    name: 'LoginRegisterJsonSchema',
+    name: '登录(JsonSchema)',
     Component: LoginRegisterJsonSchema,
   },
   {
     path: '/login-register-jsx-demo',
-    name: 'LoginRegisterJsxDemo',
+    name: '登录(JSX)',
     Component: LoginRegisterJsxDemo,
   },
   {
     path: '/login-register-register-markup-schema',
     name: '新用户注册(MarkupSchema)',
     Component: LoginRegisterRegisterMarkupSchema,
+  },
+  {
+    path: '/login-register-forgot-password-markup-schema',
+    name: '忘记密码(MarkupSchema)',
+    Component: LoginRegisterForgotPasswordMarkupSchema,
   },
 ];
 
